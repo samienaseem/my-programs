3a67b6debfb78ed698658c2b0ea07684b7f1ce7d
samienaseem <31252335+samienaseem@users.noreply.github.com>
2017-10-26 22:33:59 +0530

    my code
    just for funn cpp

---
+++ rakshabhandhan.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class raksha {
+	int money;
+	public:
+		void input()
+		{
+			cout<<"enter the money:";
+			cin>>money;
+		}
+		void output()
+		{
+			cout<<"--total money is-----"<<money;
+		}
+		void getmoney(raksha *a,raksha *s)
+		{
+			money+=a->money+s->money;
+			a->money=0;
+			s->money=0;
+		}
+};
+int main()
+{
+	raksha s1,b1,b2;
+	s1.input();
+	b1.input();
+	b2.input();
+	s1.getmoney(&b1,&b2);
+	s1.output();
+	b1.output();
+	b2.output();
+}
+
